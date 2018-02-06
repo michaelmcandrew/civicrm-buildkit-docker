02e7574585a981021de7b7f7c72c6f96478b94a8
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-02-06 15:13:31 +0100

    remove echo commands from init.sh

--- init.sh
+++ init.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo git clone https://github.com/civicrm/civicrm-buildkit.git $DIR/buildkit
-echo docker-compose --project-directory $DIR up -d
-echo docker-compose --project-directory $DIR run cli civi-download-tools
-echo docker-compose --project-directory $DIR run cli civibuild cache-warmup
-echo mkdir $DIR/buildkit/.amp
-echo cp $DIR/cli/amp.services.yml $DIR/buildkit/.amp/services.yml
-echo cp $DIR/cli/civibuild.conf $DIR/buildkit/app/civibuild.conf
-echo cp $DIR/cli/nginx-vhost.php $DIR/buildkit/.amp/nginx-vhost.php
-echo rm $DIR/buildkit/app/civicrm.settings.d/100-mail.php
+sudo chown $USER:$USER $DIR/buildkit
+git clone https://github.com/civicrm/civicrm-buildkit.git $DIR/buildkit
+docker-compose --project-directory $DIR up -d
+docker-compose --project-directory $DIR run cli civi-download-tools
+docker-compose --project-directory $DIR run cli civibuild cache-warmup
+mkdir $DIR/buildkit/.amp
+cp $DIR/cli/amp.services.yml $DIR/buildkit/.amp/services.yml
+cp $DIR/cli/civibuild.conf $DIR/buildkit/app/civibuild.conf
+cp $DIR/cli/nginx-vhost.php $DIR/buildkit/.amp/nginx-vhost.php
+rm $DIR/buildkit/app/civicrm.settings.d/100-mail.php
 
 # Workaround, until a version of amp that allows nginx template overrides is
 # packaged with buildkit
-echo git clone https://github.com/amp-cli/amp $DIR/buildkit/amp
-echo docker-compose --project-directory $DIR run composer install -d /buildkit/amp
-echo ln -sf $DIR/buildkit/amp/bin/amp $DIR/buildkit/bin/amp
+git clone https://github.com/amp-cli/amp $DIR/buildkit/amp
+docker-compose --project-directory $DIR run composer install -d /buildkit/amp
+ln -sf $DIR/buildkit/amp/bin/amp $DIR/buildkit/bin/amp
