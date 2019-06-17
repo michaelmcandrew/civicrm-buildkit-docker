37a3a037528c60e8e55863194893e308766868c0
Michael McAndrew <michael@3sd.io>
2019-06-17 16:19:39 +0100

    upgrade default php version to 7.2

--- publish/publish.php
+++ publish/publish.php
@@ -1,17 +1,16 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/templates/civicrm');
-$twig = new Twig_Environment($loader, array(
-));
+$twig = new Twig_Environment($loader, array());
 
-$phpVersions = ['5.6', '7.0', '7.1', '7.2'];
+$phpVersions = ['5.6', '7.0', '7.1', '7.2', '7.3'];
 
-foreach($phpVersions as $phpVersion){
-  $dir = __DIR__. "/civicrm/php{$phpVersion}";
-  if(!is_dir($dir)){
+foreach ($phpVersions as $phpVersion) {
+  $dir = __DIR__ . "/civicrm/php{$phpVersion}";
+  if (!is_dir($dir)) {
     `mkdir -p $dir`;
   }
-  foreach(glob(__DIR__ . '/templates/civicrm/*.twig') as $file){
+  foreach (glob(__DIR__ . '/templates/civicrm/*.twig') as $file) {
     $outputFile = $dir . '/' . basename($file, '.twig');
     $template = basename($file);
     file_put_contents($outputFile, $twig->render($template, ['php_version' => $phpVersion]));
