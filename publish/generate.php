d0ddcca5736da49d96f130bc78c19e81f38d23d0
Michael McAndrew <michael@3sd.io>
2019-10-08 10:15:53 +0200

    use node 10

---
+++ publish/generate.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates/civicrm');
+$twig = new Twig_Environment($loader, array());
+
+$phpVersions = ['5.6', '7.0', '7.1', '7.2', '7.3'];
+
+// Update templates
+foreach ($phpVersions as $phpVersion) {
+  echo "Generating {$phpVersion}\n";
+  $dir = __DIR__ . "/civicrm/php{$phpVersion}";
+  if (!is_dir($dir)) {
+    `mkdir -p $dir`;
+  }
+  foreach (glob(__DIR__ . '/templates/civicrm/*.twig') as $file) {
+    $outputFile = $dir . '/' . basename($file, '.twig');
+    $template = basename($file);
+    file_put_contents($outputFile, $twig->render($template, ['php_version' => $phpVersion]));
+  }
+}
--- publish/publish.php
+++ publish/publish.php
@@ -5,20 +5,10 @@ $twig = new Twig_Environment($loader, array());
 
 $phpVersions = ['5.6', '7.0', '7.1', '7.2', '7.3'];
 
+// Update templates
 foreach ($phpVersions as $phpVersion) {
   echo "Publishing {$phpVersion}\n";
   echo "- pull base image\n";
-
-  `docker pull php:$phpVersion-apache`;
-  $dir = __DIR__ . "/civicrm/php{$phpVersion}";
-  if (!is_dir($dir)) {
-    `mkdir -p $dir`;
-  }
-  foreach (glob(__DIR__ . '/templates/civicrm/*.twig') as $file) {
-    $outputFile = $dir . '/' . basename($file, '.twig');
-    $template = basename($file);
-    file_put_contents($outputFile, $twig->render($template, ['php_version' => $phpVersion]));
-  }
   echo "- build image\n";
   $command = "docker build " . __DIR__ . "/civicrm/php{$phpVersion} --no-cache -t michaelmcandrew/civicrm-buildkit:php{$phpVersion}";
   shell_exec($command);
