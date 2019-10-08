7e4a0a5b44bed1db28408ac576308df89db40f6a
Michael McAndrew <michael@3sd.io>
2019-10-08 18:03:50 +0200

    update base before build

--- publish/publish.php
+++ publish/publish.php
@@ -4,11 +4,12 @@ $loader = new Twig_Loader_Filesystem(__DIR__ . '/templates/civicrm');
 $twig = new Twig_Environment($loader, array());
 
 $phpVersions = ['5.6', '7.0', '7.1', '7.2', '7.3'];
-
+$baseFlavour = 'apache-stretch'
 // Update templates
 foreach ($phpVersions as $phpVersion) {
   echo "Publishing {$phpVersion}\n";
   echo "- pull base image\n";
+  `docker pull php:7.2-$baseFlavour`;
   echo "- build image\n";
   $command = "docker build " . __DIR__ . "/civicrm/php{$phpVersion} --no-cache -t michaelmcandrew/civicrm-buildkit:php{$phpVersion}";
   shell_exec($command);
