b428bd6a56937839b3f4de51df80039acd5a1c19
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2022-06-07 22:58:51 +0100

    update folders

--- update.php
+++ update.php
@@ -7,7 +7,16 @@
  * @package tsd/civicrm-buildkit-docker
  */
 
-if (!is_dir('publish/vendor')) {
+if (!is_dir(__DIR__ . '/publish/vendor')) {
     die("Error: application not installed - run composer install in the publish dir\n");
 }
+passthru('git -C ' . __DIR__ . ' pull');
 passthru('php publish/generate.php');
+exec('git -C ' . __DIR__ . ' status --porcelain', $status);
+if (count($status)) {
+    echo "Changes\n";
+    passthru('git -C ' . __DIR__ . ' add .');
+    passthru('git -C ' . __DIR__ . ' commit -m "update folders"');
+    passthru('git -C ' . __DIR__ . ' push');
+}
+passthru('php publish/publish.php');
