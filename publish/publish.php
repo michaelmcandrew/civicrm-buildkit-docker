85ff987fbbd419452bcbacb5a9b3fe34e4f8ece4
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2022-06-07 23:21:59 +0100

    shorter lines in php

--- publish/publish.php
+++ publish/publish.php
@@ -9,10 +9,16 @@ foreach ($phpVersions as $phpVersion) {
     echo "- pull base image\n";
     passthru("docker pull php:$phpVersion-$baseFlavour");
     echo "- build image\n";
-    passthru("docker build " . __DIR__ . "/civicrm/php{$phpVersion} --no-cache -t michaelmcandrew/civicrm-buildkit:php{$phpVersion}");
+    passthru(implode(' ', [
+      "docker build " . __DIR__ . "/civicrm/php{$phpVersion}",
+      "--no-cache -t michaelmcandrew/civicrm-buildkit:php{$phpVersion}"
+    ]));
     echo "- push image\n";
     passthru("docker push michaelmcandrew/civicrm-buildkit:php$phpVersion");
 }
 echo "push 'latest'";
-passthru("docker tag michaelmcandrew/civicrm-buildkit:php$defaultVersion michaelmcandrew/civicrm-buildkit:latest");
+passthru(implode(" ", [
+  "docker tag michaelmcandrew/civicrm-buildkit:php$defaultVersion",
+  "michaelmcandrew/civicrm-buildkit:latest"
+]));
 passthru("docker push michaelmcandrew/civicrm-buildkit:latest");
