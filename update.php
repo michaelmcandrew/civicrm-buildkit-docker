406ee6a068cbe66c2f26a4aaa2c4a79324c85059
Michael McAndrew <michael@3sd.io>
2023-11-20 09:57:18 +0100

    update all three origins

--- update.php
+++ update.php
@@ -8,15 +8,17 @@
  */
 
 if (!is_dir(__DIR__ . '/publish/vendor')) {
-    die("Error: application not installed - run composer install in the publish dir\n");
+  die("Error: application not installed - run composer install in the publish dir\n");
 }
 passthru('git -C ' . __DIR__ . ' pull');
 passthru('php ' . __DIR__ . '/publish/generate.php');
 exec('git -C ' . __DIR__ . ' status --porcelain', $status);
 if (count($status)) {
-    echo "Changes\n";
-    passthru('git -C ' . __DIR__ . ' add .');
-    passthru('git -C ' . __DIR__ . ' commit -m "Updating docker artifacts from templates"');
-    passthru('git -C ' . __DIR__ . ' push');
+  echo "Changes\n";
+  passthru('git -C ' . __DIR__ . ' add .');
+  passthru('git -C ' . __DIR__ . ' commit -m "Updating docker artifacts from templates"');
+  passthru('git -C ' . __DIR__ . ' push lab');
+  passthru('git -C ' . __DIR__ . ' push hub');
+  passthru('git -C ' . __DIR__ . ' push 3sd');
 }
 passthru('php ' . __DIR__ . '/publish/publish.php');
