54ca1f3a9923a03815f9c93dd82225acc27c3ef4
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2023-11-21 08:30:52 +0100

    push to all three master branches

--- update.php
+++ update.php
@@ -8,17 +8,17 @@
  */
 
 if (!is_dir(__DIR__ . '/publish/vendor')) {
-  die("Error: application not installed - run composer install in the publish dir\n");
+    die("Error: application not installed - run composer install in the publish dir\n");
 }
 passthru('git -C ' . __DIR__ . ' pull');
 passthru('php ' . __DIR__ . '/publish/generate.php');
 exec('git -C ' . __DIR__ . ' status --porcelain', $status);
 if (count($status)) {
-  echo "Changes\n";
-  passthru('git -C ' . __DIR__ . ' add .');
-  passthru('git -C ' . __DIR__ . ' commit -m "Updating docker artifacts from templates"');
-  passthru('git -C ' . __DIR__ . ' push lab');
-  passthru('git -C ' . __DIR__ . ' push hub');
-  passthru('git -C ' . __DIR__ . ' push 3sd');
+    echo "Changes\n";
+    passthru('git -C ' . __DIR__ . ' add .');
+    passthru('git -C ' . __DIR__ . ' commit -m "Updating docker artifacts from templates"');
+    passthru('git -C ' . __DIR__ . ' push civi master');
+    passthru('git -C ' . __DIR__ . ' push hub master');
+    passthru('git -C ' . __DIR__ . ' push 3sd master');
 }
 passthru('php ' . __DIR__ . '/publish/publish.php');
